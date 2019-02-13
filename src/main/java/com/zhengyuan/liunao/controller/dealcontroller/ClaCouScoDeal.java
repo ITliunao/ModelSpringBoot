f2c5920bb79bffd0fb15442898a9c501e4cd4a62
wonderfulMorty <luroot153@gmail.com>
2019-02-13 17:58:14 +0800

    sadsFD

--- src/main/java/com/zhengyuan/liunao/controller/dealcontroller/ClaCouScoDeal.java
+++ src/main/java/com/zhengyuan/liunao/controller/dealcontroller/ClaCouScoDeal.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSON;
+import com.zhengyuan.liunao.entity.ClaCouSco;
 import com.zhengyuan.liunao.service.ClaCouScoService;
 import com.zhengyuan.liunao.tools.Layui;
 
@@ -18,10 +20,10 @@ public class ClaCouScoDeal {
 	ClaCouScoService claCouScoService;
 
 	@RequestMapping("/getClaCouSco")
-	public String getClaCouSco(String classid) {
-		System.out.println(classid);
-		List list = claCouScoService.findStusScores("001", "001", "语文");
+	public String getClaCouSco(@RequestParam("key[grade]") String grade, @RequestParam("key[cla]") String cla,
+			@RequestParam("key[coursename]") String coursename) {
+		List<ClaCouSco> list = claCouScoService.findStusScores("001", "001", "语文");
 		Layui l = Layui.data(list.size(), list);
-		return JSON.toJSONString(l.toString());
+		return JSON.toJSONString(l);
 	}
 }
